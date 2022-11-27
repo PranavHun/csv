549847802d3d205af7c35a5afd3265a68454a794
Pranav Hundekari <@PranavHun>
2022-11-28 00:30:39 +0530

    Removed sample.csv from git

--- csv.c
+++ csv.c
@@ -4,8 +4,7 @@
 #include <stdlib.h>
 
 ERRCODE csv_read(CSVFILE *data, const char *filename, const char separator_line,
-                 const char separator_column)
-{
+                 const char separator_column) {
   FILE *csv_file = fopen(filename, "r");
   if (!csv_file)
     return FILE_NOT_FOUND;
@@ -34,25 +33,22 @@ ERRCODE csv_read(CSVFILE *data, const char *filename, const char separator_line,
   data->cell_start = (size_t *)malloc(sizeof(size_t) * cell_start_size);
   data->cell_start[cell_start_counter] = 0;
 
-  for (size_t i = 0; i < filesize; ++i)
-  {
+  for (size_t i = 0; i < filesize; ++i) {
     ch = fgetc(csv_file);
 
-    if (ch == separator_column || ch == separator_line)
-    {
-      if (ch == separator_line)
-      {
+    if (ch == separator_column || ch == separator_line) {
+      if (ch == separator_line) {
         data->number_of_rows++;
         if (data->number_of_columns == 0)
           data->number_of_columns = column_count_check;
         else if (data->number_of_columns != column_count_check)
-          printf("Error while parsing CSV file! %zd, %zd\n", data->number_of_columns, column_count_check);
+          printf("Error while parsing CSV file! %zd, %zd\n",
+                 data->number_of_columns, column_count_check);
         column_count_check = 0;
       }
       ++column_count_check;
       data->number_of_cells++;
-      if (cell_start_counter >= cell_start_size)
-      {
+      if (cell_start_counter >= cell_start_size) {
         cell_start_size += BUFFER_INCREMENT;
         data->cell_start = (size_t *)realloc(data->cell_start,
                                              sizeof(size_t) * cell_start_size);
@@ -72,10 +68,10 @@ ERRCODE csv_read(CSVFILE *data, const char *filename, const char separator_line,
   return OKAY;
 }
 
-char *csv_read_cell_to_str(CSVFILE *data, size_t row, size_t col)
-{
-  if (row < data->number_of_rows && col < data->number_of_columns)
-  {
+char **csv_read_row(CSVFILE *data, size_t row) {}
+
+char *csv_read_cell_to_str(CSVFILE *data, size_t row, size_t col) {
+  if (row < data->number_of_rows && col < data->number_of_columns) {
     size_t cell_number = row * data->number_of_columns + col;
     size_t len =
         data->cell_start[cell_number + 1] - 1 - data->cell_start[cell_number];
@@ -83,15 +79,12 @@ char *csv_read_cell_to_str(CSVFILE *data, size_t row, size_t col)
     strncpy(value, data->file_string + data->cell_start[cell_number], len);
     value[len] = '\0';
     return value;
-  }
-  else
-  {
+  } else {
     return NULL;
   }
 }
 
-void csv_free(CSVFILE *data)
-{
+void csv_free(CSVFILE *data) {
   free(data->cell_start);
   free(data->file_string);
 }
--- data/csvcreator.py
+++ data/csvcreator.py
@@ -1,5 +1,5 @@
 with open("data/sample.csv", "w") as csvfile:
-    csvfile.writelines(",".join([f"head{i:02d}" for i in range(1, 6)]) + "\n")
-    for i in range(1, 10):
+    csvfile.writelines(",".join([f"head{i:d}" for i in range(1, 200)]) + "\n")
+    for i in range(1, 6000):
         csvfile.writelines(
-            ",".join([f"cell{i}{j}" for j in range(1, 6)]) + "\n")
+            ",".join([f"cell{i}{j}" for j in range(1, 2000)]) + "\n")
